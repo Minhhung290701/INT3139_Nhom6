0d1dfa70d5788498c4146ac41522a6fe00a6c04b
Hùng Tăng <minhhung290701@gmail.com>
2022-10-29 10:27:22 +0700

    V1

---
+++ config/keyloginFB.js
@@ -0,0 +1,7 @@
+module.exports={
+  "facebook_api_key"      :     "2942736796026699",
+  "facebook_api_secret"   :     "f1ef3fa3c2cf29405d7673b0b7212fc1",
+  "callback_url"          :     "http://localhost:3000/auth/facebook/callback"
+}
+
+
---
+++ config/keyloginGG.js
@@ -0,0 +1,11 @@
+// add this file to .gitignore
+
+module.exports = {
+   google: {
+      clientID: '688068307698-dj5vs8ie3qo4r568j1ied4skevt98qj3.apps.googleusercontent.com',
+      clientSecret: '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'
+   },
+   session: {
+      cookieKey: 'tangminhhung'
+   }
+}
